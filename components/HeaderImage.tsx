e97b083bdf95cdc1055903b3ef5236d0ebf5f8c6
Hoàng Anh <nth.anh020209@gmail.com>
2025-02-08 16:22:58 +0700

    xoa package khong can thiet

--- components/HeaderImage.tsx
+++ components/HeaderImage.tsx
@@ -1,4 +1,3 @@
-import { LinearGradient } from 'expo-linear-gradient';
 import { View, Text, Image } from 'react-native';
 import React from 'react';
 import bg from '@/assets/images/background.jpg';
@@ -7,10 +6,6 @@ const HeaderImage = ({ text }: { text: string }) => {
   return (
     <View className='relative w-full'>
       <Image source={bg} className='z-0 w-full h-[200px]' />
-      {/* <LinearGradient
-        colors={['transparent', '#fff']}
-        className='absolute h-[200px] left-0 right-0 top-0'
-      /> */}
       <Text className='text-3xl text-white font-semibold absolute bottom-16 left-5'>
         {text}
       </Text>
